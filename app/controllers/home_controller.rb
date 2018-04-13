ebc2f84710f8e26cb384ada5cb20444d4e1d3191
EC2 Default User <ec2-user@ip-172-31-16-110.us-east-2.compute.internal>
2018-04-13 16:26:11 +0000

    another commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,64 +6,64 @@ class HomeController < ApplicationController
   end
 
   def show
-    @bon=0
-    @num=[]
-    @a=[]
-    @a=(1..45).to_a.sample(6).sort
-    @b=[]
+    @random_num=[]
+    @random_num=(1..45).to_a.sample(6).sort
+    @drwtNo=[]
+    @bnusNo=0
+    @count_num=[]#똑같은 번호
     @lotto_hash = JSON.parse(open('http://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo=801').read).sort
+    
+    #drwtNo 로또번호
     @lotto_hash.each do |k,v|
-    if k.include?('drwtNo')
-      @b<<v
+      if k.include?('drwtNo')
+        @drwtNo<<v
+      end
     end
-    end
+    
+    #bnusNo를 로또번호에 넣음
     @lotto_hash.each do |k,v|
       if k.include?('bnusNo')
-        @b<<v
-        @bon=v
+        @drwtNo<<v
+        @bnusNo=v
       end
     end
+    
+    
+    
+    #random번호와 로또번호가 맞으면 count를 세아림
     @count=0
-    @a.each do |q|
-      @b.each do |w|
+    @drwtNo.each do |q|
+      @random_num.each do |w|
         if q==w
-          @count+=1
-          @num<<q
+          @count = @count+1
+          @count_num<<q
         end
       end
     end
-    @b.pop
-    @count_n=0
-    @rank=0
-    @a.each do |q|
-      @b.each do |w|
-        if q==w
-          @count_n+=1
-        end
-      end
+    
+    @rank=99999
+    @include_bnusNo = @random_num.include?(@bnusNo)
+    
+    if @count==6
+      if @include_bnusNo==false
+       @rank=1
+     else @rank=2
     end
-    if @count_n == 6
-      @rank=1
+  end
+    
+    if @count==5
+       @rank=3
     end
     
-    if @count_n==5
-      @rank=3
-    end
-    
-    if @count_n==4
+    if @count==4
       @rank=4
     end
     
-    if @count_n==3
+    if @count==3
       @rank=5
     end
      
-    if @count==6 && @count_n==5
-      @rank=2
-    end
   end
 
-
-  
   
 end
